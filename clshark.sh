d5e46796a6d15104f60b89be1040757f36050c37
yamause <yamause.devnezumi@gmail.com>
2024-08-10 00:28:32 +0900

    Update cshark.sh

--- clshark.sh
+++ clshark.sh
@@ -4,14 +4,22 @@ containerlab_host_address=$1
 lab_node_name=$2
 if_name=$3
 
-if [[ -v WIRESAHRK_PATH ]]; then
-	wireshrk_path="$WIRESAHRK_PATH"
-elif [ -f /proc/sys/fs/binfmt_misc/WSLInterop ]; then
-	wireshrk_path="/mnt/c/Program\ Files/Wireshark/Wireshark.exe"
+wireshark_path=$WIRESAHRK_PATH
+
+if [[ -f $wireshark_path ]]; then
+	:
+# Use WSL.
+elif [[ -f /mnt/c/Program\ Files/Wireshark/Wireshark.exe ]]; then
+	wireshark_path="/mnt/c/Program\ Files/Wireshark/Wireshark.exe"
+# Use MacOS or Linux.
+elif [[ -f /usr/local/bin/wireshark ]]; then
+	wireshark_path="/usr/local/bin/wireshark"
 else
-	wireshrk_path="wireshark"
+	echo "Wireshark not found. Install Wireshark, or if it is already installed, set the path in the environment variable WIRESAHRK_PATH." >&2
+	exit 1
 fi
 
 ssh $containerlab_host_address \
 "sudo ip netns exec $lab_node_name tcpdump -U -nni $if_name -w -" | \
-eval $wireshrk_path -k -i -
+eval $wireshark_path -k -i -
+
