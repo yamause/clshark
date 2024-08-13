2fed1a36145d168889e35b6880314874957801c9
yamause <yamause.devnezumi@gmail.com>
2024-08-13 21:54:50 +0900

    tab to 2 space

--- clshark.sh
+++ clshark.sh
@@ -7,16 +7,16 @@ if_name=$3
 wireshark_path=$WIRESAHRK_PATH
 
 if [[ -f $wireshark_path ]]; then
-	:
+  :
 # Use WSL.
 elif [[ -f /mnt/c/Program\ Files/Wireshark/Wireshark.exe ]]; then
-	wireshark_path="/mnt/c/Program\ Files/Wireshark/Wireshark.exe"
+  wireshark_path="/mnt/c/Program\ Files/Wireshark/Wireshark.exe"
 # Use MacOS or Linux.
 elif [[ -f /usr/local/bin/wireshark ]]; then
-	wireshark_path="/usr/local/bin/wireshark"
+  wireshark_path="/usr/local/bin/wireshark"
 else
-	echo "Wireshark not found. Install Wireshark, or if it is already installed, set the path in the environment variable WIRESAHRK_PATH." >&2
-	exit 1
+  echo "Wireshark not found. Install Wireshark, or if it is already installed, set the path in the environment variable WIRESAHRK_PATH." >&2
+  exit 1
 fi
 
 ssh $containerlab_host_address \
